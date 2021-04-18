b2c82e4ba43a59310413e6e49c875a691d6a45bc
akhil117 <akhilbatchu123@gmail.com>
2021-04-18 12:36:04 +0530

    Added Functionality for Tabs and Menu

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -60,11 +60,11 @@ const useStyles = makeStyles(theme => ({
     menuItem: {
         ...theme.typography.tab,
         opacity: 0.7,
-        "&:hover":{
+        "&:hover": {
             opacity: 1
         }
     }
-    
+
 
 }));
 
@@ -78,6 +78,7 @@ const Header = () => {
     const [value, setValue] = useState(0);
     const [anchorEl, setAnchorEl] = useState(null);
     const [open, setOpen] = useState(false);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -88,32 +89,78 @@ const Header = () => {
         setAnchorEl(event.currentTarget);
     };
 
+    const handleMenuItemClick = (event, index) => {
+        setSelectedIndex(index);
+        setAnchorEl(null);
+    };
+
     const handleClose = () => {
         setOpen(false);
         setAnchorEl(null);
     };
 
     useEffect(() => {
-        if (window.location.pathname === "/" && value!==0){
-            setValue(0)
-        }
-        else if (window.location.pathname === "/services" && value !== 1) {
-            setValue(1);
-        }
-        else if (window.location.pathname === "/revolution" && value !== 2) {
-            setValue(2);
-        }
-        else if (window.location.pathname === "/about" && value !== 3) {
-            setValue(3);
-        }
-        else if (window.location.pathname === "/contact" && value !== 4) {
-            setValue(4);
-        }
-        else if (window.location.pathname === "/estimate" && value !== 5) {
-            setValue(5);
+        switch (window.location.pathname) {
+            case "/":
+                if (value !== 0) {
+                    setValue(0)
+                }
+                break;
+            case "/services":
+                if (value !== 1) {
+                    setValue(1);
+                    setSelectedIndex(0);
+                }
+                break;
+            case "/customsoftware":
+                if (value !== 1) {
+                    setValue(1);
+                    setSelectedIndex(1);
+                }
+                break;
+            case "/mobileapps":
+                if (value !== 1) {
+                    setValue(2);
+                    setSelectedIndex(2);
+                }
+                break;
+            case "/websites":
+                if (value !== 1) {
+                    setValue(1);
+                    setSelectedIndex(3);
+                }
+                break;
+            case "/revolution":
+                if (value !== 2) {
+                    setValue(2);
+                }
+                break;
+            case "/about":
+                if (value !== 3) {
+                    setValue(3);
+                }
+                break;
+            case "/contact":
+                if (value !== 4) {
+                    setValue(4);
+                }
+                break;
+            case "/estimate":
+                if (value !== 5) {
+                    setValue(5);
+                }
+                break;
+            default:
+                setValue(0)
+                break;
         }
     }, [value])
-
+    const menuProperties = [
+        { name: "Services", url: "/services" },
+        { name: "Custom Software Development", url: "/customsoftware" },
+        { name: "Mobile App Development", url: "/mobileapps" },
+        { name: "Website Development", url: "/websites" }
+    ]
     return (
         <React.Fragment>
             <ElevationScroll>
@@ -190,16 +237,17 @@ const Header = () => {
                             keepMounted
                             open={open}
                             onClose={handleClose}
-                            classes={{paper:classes.menu}}
+                            classes={{ paper: classes.menu }}
                             MenuListProps={{
                                 onMouseLeave: handleClose,
                             }}
                             elevation={0}
                         >
-                            <MenuItem classes={{root:classes.menuItem}} component={Link} to="/services" onClick={() => { handleClose(); setValue(1) }}>Services</MenuItem>
-                            <MenuItem classes={{root:classes.menuItem}} component={Link} to="/customsoftware" onClick={() => { handleClose(); setValue(1) }}>Custom Software Development</MenuItem>
-                            <MenuItem classes={{root:classes.menuItem}} component={Link} to="/mobileapps" onClick={() => { handleClose(); setValue(1) }}>Mobile App Development</MenuItem>
-                            <MenuItem classes={{root:classes.menuItem}} component={Link} to="/websites" onClick={() => { handleClose(); setValue(1) }}>Website Development</MenuItem>
+                            {
+                                menuProperties.map((option, index) => (
+                                    <MenuItem key={option} selected={index === selectedIndex && value === 1} classes={{ root: classes.menuItem }} component={Link} to={option.url} onClick={(event) => { handleClose(); setValue(1); handleMenuItemClick(event, index) }}>{option.name}</MenuItem>
+                                ))
+                            }
                         </Menu>
                     </Toolbar>
                 </AppBar>
